d6a527edeec6f69c80f05c046b815f23fc45c2ad
Minh Thien <minhthien.2001.nguyen@gmail.com>
2024-06-16 21:13:45 +0700

    init project

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,10 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World, Learn Java with MT");
+		System.out.println("Hello World, Learn Java with MT");
+		System.out.println("Hello World, Learn Java with MT");
+		System.out.println("Hello World, Learn Java with MT");
+	}
+}
